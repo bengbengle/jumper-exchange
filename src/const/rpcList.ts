f09a467c21cd9b2e1bf1ee7339ba29eb606b0489
Terry <43799882+bengbengle@users.noreply.github.com>
2024-11-01 16:48:44 +0800

    update rpc

--- src/const/rpcList.ts
+++ src/const/rpcList.ts
@@ -2,26 +2,28 @@ import { shuffleArray } from 'src/utils/shuffleArray';
 
 const optRPCList = [
   'https://mainnet.optimism.io',
-  'https://op-pokt.nodies.app',
+  'https://lb.nodies.app/v1/3606eebc8e2a4935a77619545f300d8a',
   // 'https://optimism.drpc.org',
-  `https://lb.drpc.org/ogrpc?network=optimism&dkey=${process.env.NEXT_PUBLIC_DKEY}`,
-  'https://optimism.meowrpc.com',
+  // `https://lb.drpc.org/ogrpc?network=optimism&dkey=${process.env.NEXT_PUBLIC_DKEY}`,
+  // 'https://optimism.meowrpc.com',
 ];
 
 const arbRPCList = [
   'https://arb1.arbitrum.io/rpc',
+  'https://lb.nodies.app/v1/ea394185e55e4f0596447e844d5bcf60',
+
   // 'https://arbitrum.drpc.org',
-  `https://lb.drpc.org/ogrpc?network=arbitrum&dkey=${process.env.NEXT_PUBLIC_DKEY}`,
-  'https://arb-pokt.nodies.app',
-  'https://arbitrum.meowrpc.com',
+  // `https://lb.drpc.org/ogrpc?network=arbitrum&dkey=${process.env.NEXT_PUBLIC_DKEY}`,
+  // 'https://arbitrum.meowrpc.com',
 ];
 
 const basRPCList = [
   'https://mainnet.base.org/',
-  'https://base.meowrpc.com',
-  // 'https://base.drpc.org',
-  `https://lb.drpc.org/ogrpc?network=base&dkey=${process.env.NEXT_PUBLIC_DKEY}`,
-  'https://base-pokt.nodies.app',
+  'https://lb.nodies.app/v1/5e931fc9d78449dbb8b954d9e760a03d',
+
+  // 'https://base.meowrpc.com',
+  // // 'https://base.drpc.org',
+  // `https://lb.drpc.org/ogrpc?network=base&dkey=${process.env.NEXT_PUBLIC_DKEY}`,
 ];
 
 export const publicRPCList = {
